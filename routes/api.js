22ff72c8efeb6365e9e4dc0b4f67b093541f40ee
Abdallah200444 <lyb25707@gmail.com>
2026-04-27 01:27:05 +0300

    fix controller path

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { getInfo, streamVideo } from "../controllers/api.js";
+import { getInfo, streamVideo } from "../controllers/videoController.js";
 
 const router = express.Router();
 
 router.post("/info", getInfo);
-
-// 🔥 الجديد فقط
 router.get("/stream", streamVideo);
 
 export default router;
